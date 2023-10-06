853f08a9b201dc864abc8accb7254d3cc7b8121f
DiegoSalazar <d.salazararp@gmail.com>
2023-10-05 23:21:34 -0300

    Se agrega componente de svelte y react

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script>
+  const current = new Date();
+  const currentHour = current.getHours();
+
+  let greeting = "";
+
+  if (currentHour < 12) {
+    greeting = "Good morning";
+  } else if (currentHour < 18) {
+    greeting = "Good afternoon";
+  } else {
+    greeting = "Good evening";
+  }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
