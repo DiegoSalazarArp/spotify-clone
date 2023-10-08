ee9a8ecc50568e6003dbbea4a1ee15ff06fe0e5c
DiegoSalazar <d.salazararp@gmail.com>
2023-10-07 23:28:07 -0300

    Se añade func. volume, play y sinc estado

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,32 @@
+import { usePlayerStore } from '@/store/playerStore'
+import { Pause, Play } from './Player'
+
+
+
+export function CardPlayButton({ id }) {
+
+  const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } = usePlayerStore(state => state)
+
+  const isPlayingPlaylist = isPlaying && currentMusic.playlist.id === id
+
+  const handleClick = () => {
+    if (isPlayingPlaylist) {
+      setIsPlaying(false)
+      return
+    }
+    fetch(`/api/get-info-playlist.json?id=${id}`).then(res => res.json())
+      .then(data => {
+        const { songs, playlist } = data
+        setIsPlaying(!isPlaying)
+        setCurrentMusic({ songs, playlist, song: songs[0] })
+
+        console.log({ playlist, songs })
+      })
+  }
+
+  return (
+    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
+      {isPlayingPlaylist ? <Pause /> : <Play />}
+    </button>
+  )
+}
