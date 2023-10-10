2b1c7a84d207e22ebf46e1715044a78f9772f22f
DiegoSalazar <d.salazararp@gmail.com>
2023-10-09 23:04:13 -0300

    Se agrega detalle de playlist y tambien boton play

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -3,7 +3,7 @@ import { Pause, Play } from './Player'
 
 
 
-export function CardPlayButton({ id }) {
+export function CardPlayButton({ id, size = 'small' }) {
   const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } = usePlayerStore(state => state)
   const isPlayingPlaylist = isPlaying && currentMusic.playlist.id === id
 
@@ -22,9 +22,12 @@ export function CardPlayButton({ id }) {
       })
   }
 
+  const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-6 h-6'
+
   return (
-    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
-      {isPlayingPlaylist ? <Pause /> : <Play />}
+    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4
+    hover:scale-105 transition hover:bg-green-400 ">
+      {isPlayingPlaylist ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
     </button>
   )
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -18,6 +18,54 @@ export const Volume = () => (
   <svg fill="currentColor" role="presentation" height="16" width="16" aria-hidden="true" aria-label="Volumen alto" id="volume-icon" viewBox="0 0 16 16"><path d="M9.741.85a.75.75 0 0 1 .375.65v13a.75.75 0 0 1-1.125.65l-6.925-4a3.642 3.642 0 0 1-1.33-4.967 3.639 3.639 0 0 1 1.33-1.332l6.925-4a.75.75 0 0 1 .75 0zm-6.924 5.3a2.139 2.139 0 0 0 0 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 0 1 0 4.88z"></path><path d="M11.5 13.614a5.752 5.752 0 0 0 0-11.228v1.55a4.252 4.252 0 0 1 0 8.127v1.55z"></path></svg>
 )
 
+
+const SongControl = ({ audio }) => {
+  const [currentTime, setCurrentTime] = useState(0)
+
+  useEffect(() => {
+    audio.current.addEventListener('timeupdate', handleTimeUpdate)
+
+    return () => audio.current.removeEventListener('timeupdate', handleTimeUpdate)
+  }, [])
+
+  const handleTimeUpdate = () => {
+    setCurrentTime(audio.current.currentTime)
+
+  }
+
+  const formatTime = (time) => {
+    if (time == null) return '00:00'
+
+    const seconds = Math.floor(time % 60)
+    const minutes = Math.floor(time / 60)
+
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`
+
+  }
+
+  const duration = audio?.current?.duration ?? 0
+
+  return (
+    <div className="flex gap-x-3 text-xs pt-2">
+      <span className="opacity-50 w-12 text-right">{formatTime(currentTime)}</span>
+      <Slider
+        defaultValue={[0]}
+        value={[currentTime]}
+        max={audio?.current?.duration ?? 0}
+        min={0}
+        className="w-[400px]"
+        onValueChange={(value) => {
+          const [newCurrentTime] = value
+          audio.current.currentTime = newCurrentTime
+        }}
+      />
+      <span className="opacity-50 w-12">
+        {duration ? formatTime(duration) : '0:00'}
+      </span>
+    </div>
+  )
+}
+
 const VolumeControl = () => {
   const volume = usePlayerStore(state => state.volume)
   const setVolume = usePlayerStore(state => state.setVolume)
@@ -35,9 +83,6 @@ const VolumeControl = () => {
       setVolume(0)
     }
   }
-
-
-
   return (
 
     <div className="flex justify-center gap-x-2">
@@ -116,15 +161,16 @@ export function Player() {
   }
 
   return (
-    <div className="flex flex-row justify-between w-full px-4 z-50">
-      <div>
+    <div className="flex flex-row justify-between w-full z-50">
+      <div className="w-[200px]">
         <CurrentSong {...currentMusic.song} />
       </div>
       <div className="grid place-content-center gap-4 flex-1">
-        <div className="flex justify-center">
+        <div className="flex justify-center flex-col items-center">
           <button className="bg-white rounded-full p-2" onClick={handleClick}>
             {isPlaying ? <Pause /> : <Play />}
           </button>
+          <SongControl audio={audioRef} />
           <audio ref={audioRef} />
         </div>
       </div>
